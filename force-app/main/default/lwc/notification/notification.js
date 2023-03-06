bc6e84bf4ea3a5e26df25017547075e786572e6e
ankit-c11 <ankitrocks1010@gmail.com>
2023-03-06 17:08:39 +0530

    notifs

---
+++ force-app/main/default/lwc/notification/notification.js
@@ -0,0 +1,23 @@
+import { LightningElement } from 'lwc';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent'
+
+export default class Notification extends LightningElement {
+    toastHandler(){
+        const evt = new ShowToastEvent({
+            title:'Success',
+            message:'Account Created!!',
+            variant:'success'
+        })
+        this.dispatchEvent(evt);
+
+    }
+    toastHandlerErr(){
+        const evt = new ShowToastEvent({
+            title:'Error',
+            message:'Error occured!',
+            variant:'error'
+        })
+        this.dispatchEvent(evt);
+
+    }
+}
